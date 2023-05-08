200f30138f6d4c76c44171dfeb08f210120aa168
EdoFro <35700575+EdoFro@users.noreply.github.com>
2023-05-08 18:58:22 -0400

    Update UserStyles.groovy
    added includeConditionalRules

--- MarkdownHelper/src/main/groovy/UserStyles.groovy
+++ MarkdownHelper/src/main/groovy/UserStyles.groovy
@@ -25,12 +25,15 @@ class UserStyles {
 
 //region: copyUserStyles
 
-    def static copyUserStyles(sourceMap, targetMap, boolean showMessage, groovy.lang.Closure closure){
+    def static copyUserStyles(sourceMap, targetMap, boolean includeConditionalRules, boolean showMessage, groovy.lang.Closure closure){
         def stylesToImport = getUserDefinedStylesParentNode(sourceMap).children.findAll(closure)
         def styleNamesToImport = stylesToImport*.text
-        def texto = new StringBuilder("The following styles were imported \n from map '${sourceMap.name}.mm' \n into current map '${targetMap.name}.mm':\n\n")
+        def texto = new StringBuilder("The following styles ${includeConditionalRules?'(including their available conditional rules) ':''}were imported \n from map '${sourceMap.name}.mm' \n into current map '${targetMap.name}.mm':\n\n")
         styleNamesToImport.each{styleName ->
             targetMap.copyStyleFrom(sourceMap, styleName)
+            if (includeConditionalRules){
+                targetMap.copyConditionalStylesFrom(sourceMap, styleName)
+            }
             def sourceStyleNode = getUserStyleNode(sourceMap, styleName )
             def targetStyleNode = getUserStyleNode(targetMap, styleName )
             copyIcons(sourceStyleNode, targetStyleNode)
@@ -41,30 +44,46 @@ class UserStyles {
         if(showMessage) ui.informationMessage(texto.toString())
     }
 
-    def static copyUserStyles(sourceMap, targetMap, boolean showMessage = true){
-        copyUserStyles(sourceMap, targetMap, showMessage, {true})
+    def static copyUserStyles(sourceMap, targetMap){
+        copyUserStyles(sourceMap, targetMap, true, true, {true})
+    }
+
+    def static copyUserStyles(sourceMap, targetMap, boolean includeConditionalRules, boolean showMessage = true){
+        copyUserStyles(sourceMap, targetMap, includeConditionalRules, showMessage, {true})
     }
 
     def static copyUserStyles(sourceMap, targetMap, groovy.lang.Closure closure){
-        copyUserStyles(sourceMap, targetMap, true, closure)
+        copyUserStyles(sourceMap, targetMap, true, true, closure)
+    }
+
+    def static copyUserStyles(sourceMap, targetMap, boolean includeConditionalRules, groovy.lang.Closure closure){
+        copyUserStyles(sourceMap, targetMap, includeConditionalRules, true, closure)
     }
 
     def static copyUserStyles(sourceMap, targetMap, String[] lista){
-        copyUserStyles(sourceMap, targetMap, true, lista)
+        copyUserStyles(sourceMap, targetMap, true, true, lista)
     }
 
-    def static copyUserStyles(sourceMap, targetMap, boolean showMessage, String[] lista){
+    def static copyUserStyles(sourceMap, targetMap, boolean includeConditionalRules, String[] lista){
+        copyUserStyles(sourceMap, targetMap, includeConditionalRules, true, lista)
+    }
+
+    def static copyUserStyles(sourceMap, targetMap, boolean includeConditionalRules, boolean showMessage, String[] lista){
         def closure = {it.text in lista}
-        copyUserStyles(sourceMap, targetMap, showMessage, closure)
+        copyUserStyles(sourceMap, targetMap, includeConditionalRules, showMessage, closure)
     }
 
     def static copyUserStyles(sourceMap, targetMap, String texto){
-        copyUserStyles(sourceMap, targetMap, true, texto)
+        copyUserStyles(sourceMap, targetMap, true, true, texto)
     }
 
-    def static copyUserStyles(sourceMap, targetMap, boolean showMessage, String texto){
+    def static copyUserStyles(sourceMap, targetMap, boolean includeConditionalRules, String texto){
+        copyUserStyles(sourceMap, targetMap, includeConditionalRules, true, texto)
+    }
+
+    def static copyUserStyles(sourceMap, targetMap, boolean includeConditionalRules, boolean showMessage, String texto){
         def closure = {it.text == texto}
-        copyUserStyles(sourceMap, targetMap, showMessage, closure)
+        copyUserStyles(sourceMap, targetMap, includeConditionalRules, showMessage, closure)
     }
 
 //end:
